2bcb041088330c81434cccdf01d91e065656e047
telthemweb <telthem.kerlyeen@gmail.com>
2023-10-13 13:45:33 +0200

    updated

--- controllers/econetController.js
+++ controllers/econetController.js
@@ -5,23 +5,19 @@ const {generateAirtimeVendorRefence,nowDate} = require("../utils/util.js");
 const { Airtime } = require("../models/airtime.js");
 const axios  =require("axios");
 const econetSouceMobile = process.env.ECONETSOURCENUMBER //"263772978751";
-
+ const url = process.env.BASE_URL;
 
 //zwl airtime
 const econetAirtimeControllerV2 = asyncHandler(async (req, res, next) => {
-   const url = process.env.BASE_URL;
     const { amount, targetMobile, payingNumber } = req.body;
     const cents = amount * 100;
-    // validate if the paying number and the targetMobile is an econet phone number
-    const econet = /^077|^078/;   // regex for econet phone number
-    const netone = /^071/;
+    const econet = /^077|^078/; 
     let method;
     var my_status;
     // const orderNumber = telgenerateRandomString(10);
 
 
     if (econet.test(payingNumber)) { method = 'ecocash' }
-    if (netone.test(payingNumber)) { method = 'onemoney' }
 
 
    if (method === "ecocash") {
@@ -40,7 +36,6 @@ const econetAirtimeControllerV2 = asyncHandler(async (req, res, next) => {
             "transmissionDate": nowDate(),
             "currencyCode": "ZWL",
         },
-        // auth object
 
         {
             auth: {
@@ -100,7 +95,7 @@ const { vendorReference, transactionAmount, utilityAccount, narrative, currencyC
 
 //usd airtime
 const econetAirtimeControllerV2USD = asyncHandler(async (req, res, next) => {
-   const url = process.env.BASE_URL;
+  
     const { usdamount, targetMobile, currencyCode } = req.body;
 
     console.log(currencyCode)
@@ -181,130 +176,12 @@ const econetAirtimeControllerV2USD = asyncHandler(async (req, res, next) => {
 
 });
 
-//bundles zwl
-const econetZwlBundlesControllerV2 = asyncHandler(async (req, res, next) => {
-   const url = process.env.BASE_URL;
-    const { amount, targetMobile, payingNumber } = req.body;
-    const cents = amount * 100;
-    // validate if the paying number and the targetMobile is an econet phone number
-    const econet = /^077|^078/;   // regex for econet phone number
-    const netone = /^071/;
-    let method;
-  
-
-
-    if (econet.test(payingNumber)) { method = 'ecocash' }
-    if (netone.test(payingNumber)) { method = 'onemoney' }
-
-
-   if (method === "ecocash") {
-    axios.post(`${url}`,
-        {
-            "mti": "0200",
-            "vendorReference": generateAirtimeVendorRefence("econet"),
-            "processingCode": "U50000",
-            "vendorNumber": vendorNumbers.econet,
-            "transactionAmount": cents,
-            "sourceMobile": econetSouceMobile,
-            "targetMobile": `263${targetMobile.slice(1)}`,
-            "utilityAccount": `263${targetMobile.slice(1)}`,
-            "merchantName": "ECONET",
-            "productName": "ECONET_BUNDLE",
-            "transmissionDate": nowDate(),
-            "currencyCode": "ZWL",
-        },
-       
-
-        {
-            auth: {
-                username: process.env.API_USERNAME,
-                password: process.env.API_PASSWORD
-            }
-        }
 
 
 
-    ).then(data => {
-
-        console.log(data.data)
-        if (data.data.responseCode === "00") {
-            res.send(data.data)
-        }
-        else{
-            console.log("General Error.. response code 05")
-            return res.json({
-                error: "err01",
-                message: data.data.narrative,
-                description: data.data
-            })
-        }
-    })
-   }
-
-
-});
-
-
-//usd bundle
-const econetUsdBundleControllerV2 = asyncHandler(async (req, res, next) => {
-   const url = process.env.BASE_URL;
-    const { usdamount, targetMobile, currencyCode } = req.body;
-
-    console.log(currencyCode)
-
-    const cents = usdamount * 100;
-    axios.post(`${url}`,
-        {
-            "mti": "0200",
-            "vendorReference": generateAirtimeVendorRefence("econet"),
-            "processingCode": "U50000",
-            "vendorNumber": vendorNumbers._liveVendorNumber,
-            "transactionAmount": cents,
-            "sourceMobile": econetSouceMobile,
-            "targetMobile": `263${targetMobile.slice(1)}`,
-            "utilityAccount": `263${targetMobile.slice(1)}`,
-            "merchantName": "ECONET",
-            "productName": "ECONET_BUNDLE",
-            "transmissionDate": nowDate(),
-            "currencyCode": currencyCode,
-            "apiVersion": "02"
-
-        },
-        // auth object
-
-        {
-            auth: {
-                username: process.env.API_USERNAME,
-                password: process.env.API_PASSWORD
-            }
-        }
-
-
-
-    ).then(data => {
-
-        console.log(data.data)
-        if (data.data.responseCode === "00") {
-            res.send(data.data)
-        }
-        else{
-            console.log("General Error.. response code 05")
-            return res.json({
-                error: "err01",
-                message: data.data.narrative,
-                description: data.data
-            })
-        }
-    })
-   
-
-
-});
 
 
 module.exports = {
 	econetAirtimeControllerV2,
 	econetAirtimeControllerV2USD,
-    econetZwlBundlesControllerV2,
-    econetUsdBundleControllerV2
 }
--- routes/econetRoute.js
+++ routes/econetRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { econetAirtimeControllerV2,econetAirtimeControllerV2USD,econetZwlBundlesControllerV2,econetUsdBundleControllerV2 }= require("../controllers/econetController");
+const { econetAirtimeControllerV2,econetAirtimeControllerV2USD }= require("../controllers/econetController");
 
 router.post("/buy/airtime/v2", econetAirtimeControllerV2);
 router.post("/buy/usd/airtime", econetAirtimeControllerV2USD);
 
-router.post("/buy/zwl/bundle", econetZwlBundlesControllerV2);
-router.post("/buy/usd/bundle", econetUsdBundleControllerV2);
+// router.post("/buy/zwl/bundle", econetZwlBundlesControllerV2);
+// router.post("/buy/usd/bundle", econetUsdBundleControllerV2);
 
 module.exports = router;
